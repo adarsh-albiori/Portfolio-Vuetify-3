ca8651a02f63629eae0eb9e4f087e13d0f44b8b4
Adarsh Parmar <adarshparmar39@gmail.com>
2023-07-12 17:13:05 +0530

    Final Commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar app color="black" dark flat class="px-12">
+    <v-btn>
+      <v-icon color="yellow" left class="mr-2"> fas fa-signature</v-icon>Adarsh
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text:true @click="scroll('home')" class="text-yellow">Home</v-btn>
+    <v-btn text:true @click="scroll('about')">About</v-btn>
+    <v-btn text:true @click="scroll('portfolio')">Portfolio</v-btn>
+    <v-btn text:true @click="scroll('services')">Services</v-btn>
+    <v-btn text:true @click="scroll('page')">Page</v-btn>
+    <v-btn text:true @click="scroll('blog')">Blog</v-btn>
+    <v-btn text:true @click="scroll('contact')">Contact</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" });
+    },
+  },
+};
+</script>
